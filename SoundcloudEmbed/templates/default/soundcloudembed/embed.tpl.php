3c631addf0bbf3660c005cb7ce8b5598d2437683
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-01 16:50:08 +0000

    Adding support for shortlink expansion. Depends on patch idno/idno#547

--- SoundcloudEmbed/templates/default/soundcloudembed/embed.tpl.php
+++ SoundcloudEmbed/templates/default/soundcloudembed/embed.tpl.php
@@ -2,7 +2,9 @@
 
 
 $embedded = '';
-if (preg_match_all('/(http:\/\/|https:\/\/)soundcloud\.com\/([^\s]+)/i', $vars['object']->body, $matches)) {
+$body = Idno\Core\site()->triggerEvent('url/expandintext', ['object' => $vars['object']], $vars['object']->body); 
+
+if (preg_match_all('/(http:\/\/|https:\/\/)soundcloud\.com\/([^\s]+)/i', $body, $matches)) {
     
     foreach ($matches[0] as $m)
         $embedded .= '<div id="sc_'.md5($m).'" class="soundcloud-embed" data-url="'.$m.'"></div>';
